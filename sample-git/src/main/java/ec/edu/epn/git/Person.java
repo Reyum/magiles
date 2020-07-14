10c4ef1faeed20c494e8be3d69f79fb870ea7c47
Reyum <59716672+Reyum@users.noreply.github.com>
2020-07-14 15:31:48 -0500

    Commit 2

--- sample-git/src/main/java/ec/edu/epn/git/Person.java
+++ sample-git/src/main/java/ec/edu/epn/git/Person.java
@@ -3,24 +3,37 @@ package ec.edu.epn.git;
 public class Person {
 	private int age;
 	private String name;
+	private String city;
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Person(int age, String name) {
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Person(int age, String name, String city) {
 		super();
 		this.age = age;
 		this.name = name;
+		this.city = city;
 	}
-	
-	
 
 }
