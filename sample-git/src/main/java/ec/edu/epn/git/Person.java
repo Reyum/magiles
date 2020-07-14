96decd1067e89194f1e1be7d6856b7e98f2e9406
Reyum <59716672+Reyum@users.noreply.github.com>
2020-07-14 15:15:06 -0500

    Mi primer commit

---
+++ sample-git/src/main/java/ec/edu/epn/git/Person.java
@@ -0,0 +1,26 @@
+package ec.edu.epn.git;
+
+public class Person {
+	private int age;
+	private String name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Person(int age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+	
+	
+
+}
